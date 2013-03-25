8cdb312c5fb6acaa1266db588b884c58a8ee6195
Jacob Newmister <jnewmis@falcon.bgsu.edu>
2013-03-25 10:54:33 -0400

    Gets a locus tag from a GI Number

---
+++ getLocusTag.py
@@ -0,0 +1,25 @@
+### Function Name: getLocusTag
+### Expected Input: giNumber of a given Alignment
+### Processing: Takes the giNumber given to the function and runs a search
+###             of the Entrez database. Returns all info and then slices the
+###             locus tag out.
+### Output: Locus Tag, if present. Else returns error message
+### Requirements: importing Entrez from Bio. Email defined in main body.
+###               (ex. Entrez.email = "youremail@goes.here")
+### Author: Newmister, Jacob
+### Comments: This file may need to be changed if the system for locus_tag
+###           storage is ever changed
+
+def getLocusTag(giNumber):
+    dbfetch = Entrez.efetch(db = "protein", id = giNumber, rettype = "gb", retstyle = "xml")
+    dbread = dbfetch.read()
+
+    if dbread.find('locus_tag') == -1:
+
+        locusTag = "Error: No Locus Tag Found"
+
+    else:
+
+        locusTag = dbread[dbread.find("locus_tag")+11:dbread.find("locus_tag")+19]
+
+    return locusTag
