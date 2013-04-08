25dfdb50d10d421825c31daaece060178140de3a
richard <rmnyonyi@gmail.com>
2013-04-07 23:54:31 -0400

    fixed the module to save RQ7  genes to the  database.

--- saveRQ7.py
+++ saveRQ7.py
@@ -1,32 +1,40 @@
+### Function Name: saveRQ7
+### Expected Input: A list that contains the sequences of RQ7a and a log file for reporting errors.
+### Processing: Going through each item in the list and save them to a table called RQ7 in the database named Genomes.
+### Output: None.All errors are found in the log file.
+### Author: Nyonyi,Richard.
+### Comments : Make sure  the login credentials for the database - genomes are set to reflect your database.(line 15)
+
 #!/usr/bin/env python
+def saveRQ7(list,_logFile):	
+	
+	import mysql.connector
+	from mysql.connector import errorcode
 
-def saveRQ7(list):
-import mysql.connector
-from mysql.connector import errorcode
-try:
- cnx = mysql.connector.connect(user='', password='',host='',database='')
-
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong your username or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exists")
-  else:
-    print(err)
-else:
-  
-	cursor = cnx.cursor()
-	_sql = ("INSERT INTO RQ7"
-	"(name) " 
-	"VALUES (%s)")
+	try:
+ 		cnx = mysql.connector.connect(user='username here', password='pasword here',host='host',database='Genomes')
+		cursor = cnx.cursor()
+		_sql = ("INSERT INTO RQ7 (id,identity) "  
+		"VALUES (%s,%s)")
  
-	for c in range (len(list)):
-	   
- 	   _name=list[c].id
-       _data = (_name)
-       cursor.execute(_sql, _data)
-    
-	cnx.commit()
-	cursor.close()
-	f.close()
-	cnx.close()
+		for c in range (len(list)):
+			_name=list[c].id
+			_data=(c+1,_name)
+			cursor.execute(_sql, _data)
+		cnx.commit()
+		cursor.close()	
+		cnx.close()
+		_logFile.write(" Data RQ7 was successfully saved \n");
+			 
+			
+	except mysql.connector.Error as err:
+  		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+  			_logFile.write( "Something is wrong your username or password \n");
+  		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+  			_logFile.write("Database does not exists \n");
+  		else:
+  			_logFile.write(str(err));
+	else:
+  
+		cnx.close()
+		
