49496cd44696c87ca48dec281eaa86845ba79b9b
richard <rmnyonyi@gmail.com>
2013-04-09 01:36:25 -0400

    fixed main modules

--- dbsave.py
+++ dbsave.py
@@ -1,44 +1,54 @@
+### Function Name: dbsave
+### Expected Input: organism name/table name , log file handler.
+### Processing: Opens the temporary file that contains results. 
+###             Save every data to the table as described in the organism name.
+### Output: None.Errors in log.
+### Requirements: importing mysql connector.            
+### Author: Nyonyi,Richard
+### Comments : Make sure  the login credentials for the database-Genomes are set to reflect your database.(line 19)           
+
+
 #!/usr/bin/env python
-
-def dbsave(tableName):
-import mysql.connector
-
-
-from mysql.connector import errorcode
-try:
- cnx = mysql.connector.connect(user='', password='',host='',database='')
-
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong your username or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exists")
-  else:
-    print(err)
-else:
-  
-	cursor = cnx.cursor()
-	_sql = ("INSERT INTO "+ tableName
-	"(RQ7id,GInum,Locus) " 
-	"VALUES (%s,%s,%s)")
- 
- 
-	_rq7id =[]
-	_locus=[]  
-	_gi=[]            
-	f = open('_tempResults.txt','r')
- 
-	for line in f:
-	   t = line.split()
-    
- 	   _rq7id= str(t[0])
- 	   _gi= str(t[1])
- 	   _locus= str(t[2])
- 	   
-       _data = (_rq7id,_gi,_locus)
-       cursor.execute(_sql, _data)
-    
-	cnx.commit()
-	cursor.close()
-	f.close()
-	cnx.close()
+def dbsave(organism,_logFile):
+	import mysql.connector
+	from mysql.connector import errorcode
+	try:
+		cnx = mysql.connector.connect(user='', password='',host='',database='')
+ 		cursor = cnx.cursor()
+ 		
+ 		name=organism.split()   #splitting organism name to obtain the second word.i.e from(thermotoga xxxxxxx) to only xxxxxxxx
+ 		tableName=name[1]
+ 		
+ 		_sql = ("INSERT INTO " + tableName + "(RQ7id,gi,locus) " 
+ 		"VALUES (%s,%s,%s)")
+ 	
+ 		_rq7id =[]
+ 		_locus=[]  
+ 		_gi=[]            
+ 		f = open('tempResults.txt','r')
+ 		
+ 		for line in f:  		 #Going through all data in the temporary file
+ 			if line.strip():      #skiping/ignoring any empty lines available
+ 				t = line.split()    # data in the file have the format(rq7id giNumber locusTag)
+				_rq7id= t[0]
+				_gi= t[1]
+				_locus= t[2]
+				_data = (_rq7id,_gi,_locus)
+				cursor.execute(_sql, _data)
+				
+	
+		cnx.commit()
+		cursor.close()
+		f.close()
+		cnx.close()
+			
+	except mysql.connector.Error as err:
+  		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+  			_logFile.write( "Something is wrong with your username or password \n");
+  		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+  			_logFile.write("Database does not exists \n");
+  		else:
+  			_logFile.write(str(err)+ '\n');
+	else:
+		cnx.close()
+			
--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-#
-#
-#
-#
-#
+#!/usr/bin/env python
+### Function Name: main.py
+### Expected Input: None.
+### Processing: Opens the fasta for the RQ7 and call a function(saveRQ7) 
+### 			to save the genes' identities to the database.
+###             For every organism in the list,runs multiple functions to obtain 
+###				locus tags for matched proteins and save to the respective database tables.
+### Output: None.Errors in log.            
+### Author: Nyonyi,Richard
+### Comments : 
+
 from Bio import SeqIO
 from Bio.Blast import NCBIXML
- 
+from saveRQ7 import saveRQ7 
+from setblast import setblast
+from getGInumber import getGInumber
+from getLocusTag import getLocusTag
+from getQCoverage import getQCoverage
+from dbsave import dbsave
 _list=[] #list for names of organisms 
 
 # adding organisms to the list
-_list.append('Thermotoga Neapolitana')
-_list.append('Thermotoga Maritima')
-_list.append('Thermotoga Elfii')
-_list.append('Thermotoga Hypogea')
-_list.append('Thermotoga Lettingae')
-_list.append('Thermotoga Naphthophila')
-_list.append('Thermotoga Petrophila')
-_list.append('Thermotoga Subterranea')
-_list.append('Thermotoga Thermarum')
+
+_list.append('Thermotoga maritima')
+_list.append('Thermotoga neapolitana')
+_list.append('Thermotoga elfii')
+_list.append('Thermotoga hypogea')
+_list.append('Thermotoga lettingae')
+_list.append('Thermotoga naphthophila')
+_list.append('Thermotoga petrophila')
+_list.append('Thermotoga subterranea')
+_list.append('Thermotoga thermarum')
 
 
 
+
+#file for storing errors and tracking the system:
+_logFile = open("log.txt", "a")
 
 
 _file="RQ7.fasta"     #Fasta file for the organism we want to find matches
@@ -31,58 +46,56 @@ handle.close()
 
 _totalGenes =len(_records) # counting total number of genes in the  file.
 
-saveRQ7(_records)        #save the genes to the database.
+saveRQ7(_records,_logFile )        #save the genes to the database.
 
-
-#file for storing errors and tracking the system:
-_logFile = open("log.txt", "w")
 
 
 for organism in range(len(_list)):				    	#for all organisms to compare with.
 	
 	_resultFile = open("tempResults.txt", "w")      	#file for storing temporary results before saving to database:
-	_geneCounter = 0	
-	_geneLimit=50
-	for genes in range(_totalGenes):				  	#for all records in the fasta file
-		setblast(_records[genes].seq,_list[organism]) 	# Sending the sequence and organism to compare with.
+	_geneCounter=1
+	_geneLimit=3
+	for genes in range(_totalGenes):  #for all records in the fasta file
+		print "gene number " , _geneCounter;
 		
-		#Fetching results from xml file
-    	result_handle = open("my_blast.xml")
-        blast_record = NCBIXML.read(result_handle)
+		setblast(_records[genes].seq,_list[organism])# Sending the sequence and organism to compare with.
 		
+		result_handle = open("my_blast.xml") #Fetching results from xml file
+		
+		blast_record = NCBIXML.read(result_handle)
 		
 		for alignment in blast_record.alignments: #Going through alignment read from the xml file
 			
 			#filtering by query coverage
 			for hsp in alignment.hsps:
-				_coverage = getQcoverage(hsp.query, len(_records[_geneCounter])) #get query coverage
-				if _coverage > 30:   #if query coverage is greater than 30 then find the GI and locus.
-			
+				_coverage = getQCoverage(hsp.query, len(_records[genes])) #get query coverage
+				if _coverage > 30:   #if query coverage is greater than 30 then find the GI and locus.			
+					
 					_giNumber=getGInumber(alignment.hit_id)  #get the gi number
-			
+					
 					_locusResult=getLocusTag(_giNumber)   #get the locus tag
-			
-					if _locusResult == "Error":    # checking if locus-tag was found
-						 _logFile.write("No locus for   " + _giNumber+'\n');
-			 
-					else:
-						 _locus=_locusResult
-			 		
-			 
-			 _resultFile.write(_geneCounter+" "+_giNumber+" "+_locus+'\n');	 writing the results to a temp file	
-			_geneCounter = _geneCounter + 1
-
+					
+					
+					
+				if _locusResult == "Error":    # checking if locus-tag was found
+					_logFile.write("No locus for   " +str( _giNumber) + '\n');
+					
+				else:	
+					
+					_resultFile.write(str(_geneCounter) +" "+ str(_giNumber) +" "+ str(_locusResult) + '\n');	 #writing the results to a temp file
 		if _geneCounter == _totalGenes:
-			dbsave(_list[organism])      #Saving results into the specified database table
-			_logFile.write("Saved all genes corresponding  to " +_list[organism]+'\n');
-			_resultFile.close	
-		
-		elif _geneCounter ==_geneLimit
-		    dbsave(_list[organism])      #Saving results into the specified database table
-			_logFile.write("Saved 50 genes corresponding  to " +_list[organism]+'\n');
-			_geneLimit = _geneLimit + 50
+			
+			dbsave(_list[organism],_logFile)      #Saving results into the specified database table
+			_logFile.write("Saved all genes corresponding  to " + _list[organism] + '\n');
+			_resultFile.close
+		elif _geneCounter ==_geneLimit:
+			print" _geneCounter ==_geneLimit:";
+			
+			dbsave(_list[organism],_logFile)      #Saving results into the specified database table
+			_logFile.write("Saved 50 genes corresponding  to " + _list[organism] + '\n');
+			_geneLimit = _geneLimit + 3
 			_resultFile.close
 			_resultFile = open("tempResults.txt", "w") 
-		
+		_geneCounter=_geneCounter + 1
 	_resultFile.close	
 _logFile.close
